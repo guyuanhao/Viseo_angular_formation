f2bf60727b66dc0c229ab6cbd9e1d45ca8c72b93
Unknown <guyuanhao@foxmail.com>
2017-11-30 14:33:00 +0100

    i dont continue the training programme since this afternoon

--- controller/mainCtrl.js
+++ controller/mainCtrl.js
@@ -1,3 +1,14 @@
 chicQcmAppModule.controller('MainCtrl', [function($scope){
-    console.log('MainCtrl has been created');
+    var self = this;
+		self.qcms = [
+			{id: 1, label: 'Javascript'},
+			{id: 2, label: 'Html 5'},
+			{id: 3, label: 'AngularJs'}
+		];
+    self.selectedQCM = {};
+
+    self.selectQCM = function(qcmSelected){
+      self.selectedQCM = qcmSelected;
+    }
+
 }]);
